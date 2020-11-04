0e66a9900482505e476c0ddbcc22cc26970a0c93
sigoden <sigoden@gmail.com>
2020-11-04 23:00:17 +0800

    update use-services

--- src/sequelize/sequelize.ts
+++ src/sequelize/sequelize.ts
@@ -1,25 +1,25 @@
 import { Sequelize, Options } from "sequelize";
 import { ServiceOption, InitOption, STOP_KEY } from "use-services";
 
-export type Args<K> = {
+export type Args = {
   database: string;
   username: string;
   password: string;
   options?: Options;
-  load: (sql: Sequelize) => Promise<K>,
+  load: (sql: Sequelize) => Promise<void>;
 };
 
-export type Option<K, S extends Service> = ServiceOption<Args<K>, S & K>;
+export type Option<S extends Service> = ServiceOption<Args, S>;
 
-export async function init<K, S extends Service>(
-  option: InitOption<Args<K>, S> 
-): Promise<S & K> {
+export async function init<S extends Service>(
+  option: InitOption<Args, S> 
+): Promise<S> {
   const { database, username, password, options } = option.args;
   const srv = new (option.ctor || Service)(database, username, password, options);
   await srv.authenticate();
   const models = await option.args.load(srv);
   Object.assign(srv, models);
-  return srv as S & K;
+  return srv as S;
 }
 
 export class Service extends Sequelize {
